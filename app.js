fa6d5e30cf632538ab9ccf542e190145c7a89401
Cher Lor <chercllor@gmail.com>
2021-11-14 23:22:45 -0800

    update change

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-window.addEventListener('DOMContentLoaded', () => {
-  const audioElement = document.querySelector('#audioElement');
-  const play = document.querySelector('.play');
-  const timeSelect = document.querySelectorAll('.time-select button');
-  const outline = document.querySelector('.outline-track circle');
-  const displayTimer = document.querySelector('.time-display');
+window.addEventListener("DOMContentLoaded", () => {
+  const audioElement = document.querySelector("#audioElement");
+  const play = document.querySelector(".play");
+  const timeSelect = document.querySelectorAll(".time-select button");
+  const outline = document.querySelector(".outline-track circle");
+  const displayTimer = document.querySelector(".time-display");
+  const tracks = [...document.querySelectorAll(".song-select button")];
 
   const outlineLength = outline.getTotalLength();
 
@@ -11,12 +12,51 @@ window.addEventListener('DOMContentLoaded', () => {
   outline.style.strokeDasharray = outlineLength;
 
   let playTime = 30;
+  let currentBackground =
+    "143160-wallpaper-buddha-buddhism-meditation-harmony-silhouette-hd.jpg";
+  let currentSong = "JEK9DZZ-the-meditation.mp3";
+
+  //Change track
+  tracks.forEach((track) => {
+    track.addEventListener("click", (e) => {
+      // remove tracks active class
+      removeActiveClass();
+      // append the active class
+      e.target.classList.add("active");
+      // pause the current play track
+      audioElement.pause();
+      // change the play icon
+      play.src = "/assets/svg/play.svg";
+      // set new track and background
+      currentSong = e.target.dataset.song;
+      currentBackground = e.target.dataset.bg;
+      audioElement.src = "/assets/" + currentSong;
+      resetOutline();
+      setBackground();
+    });
+  });
+
+  function removeActiveClass() {
+    tracks.forEach((track) => track.classList.remove("active"));
+  }
+  function setBackground() {
+    const bodyElement = document.querySelector("body");
+    bodyElement.style.backgroundImage = `url(/assets/${currentBackground})`;
+
+    console.log(
+      window.getComputedStyle(bodyElement).getPropertyValue("--main-color")
+    );
+  }
+  function resetOutline() {
+    outline.style.strokeDashoffset = outlineLength;
+    outline.style.strokeDasharray = outlineLength;
+  }
 
   timeSelect.forEach((btn) => {
-    btn.addEventListener('click', (e) => {
+    btn.addEventListener("click", (e) => {
       if (!audioElement.paused) {
         audioElement.pause();
-        play.src = '/assets/svg/play.svg';
+        play.src = "/assets/svg/play.svg";
       }
       playTime = +e.target.dataset.time;
       displayTimer.textContent = `${playTime / 60}:00`;
@@ -24,17 +64,17 @@ window.addEventListener('DOMContentLoaded', () => {
     });
   });
 
-  play.addEventListener('click', () => {
+  play.addEventListener("click", () => {
     checkPlaying(audioElement);
   });
 
   const checkPlaying = (audio) => {
     if (audio.paused) {
       audio.play();
-      play.src = '/assets/svg/pause.svg';
+      play.src = "/assets/svg/pause.svg";
     } else {
       audio.pause();
-      play.src = '/assets/svg/play.svg';
+      play.src = "/assets/svg/play.svg";
     }
   };
 
@@ -52,8 +92,8 @@ window.addEventListener('DOMContentLoaded', () => {
     displayTimer.textContent = `${minute}:${String(second).padStart(2, 0)}`;
 
     if (currentPlayTime >= playTime) {
-      displayTimer.textContent = '0:00';
-      play.src = '/assets/svg/play.svg';
+      displayTimer.textContent = "0:00";
+      play.src = "/assets/svg/play.svg";
       audioElement.pause();
       audioElement.currentTime = 0;
       outline.style.strokeDashoffset = outlineLength;
