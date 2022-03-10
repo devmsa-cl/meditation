8dc74bc1ed0fddc1a6be3b436e7fcfcffef6964b
ilormsa <chercllor@gmail.com>
2022-03-10 08:29:53 -0800

    add new data track

---
+++ track.js
@@ -0,0 +1,13 @@
+export default track = [
+  {
+    id: 1,
+    path: 'JEK9DZZ-the-meditation.mp3',
+    background_image:
+      '143160-wallpaper-buddha-buddhism-meditation-harmony-silhouette-hd.jpg',
+  },
+  {
+    id: 2,
+    path: 'melody-of-nature-main-6672.mp3',
+    background_image: 'Y7isDB.jpg',
+  },
+];
